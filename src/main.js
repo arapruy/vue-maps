c316aa5cdc326ab4fe8822947935fe81784458c5
arapruy <72084746+arapruy@users.noreply.github.com>
2021-10-22 07:55:33 -0300

    #4 add registering plugins

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import svgJs from "./plugin/vueSvgPlugin"
+import Notifications from 'vue-notification'
 
+Vue.use(svgJs);
+Vue.use(Notifications);
 Vue.config.productionTip = false
 
 new Vue({
